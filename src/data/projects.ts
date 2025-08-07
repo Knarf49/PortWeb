daa1df2a253370821979403c0f4ea4fac6e37d77
Frank <wasutha1449@gmail.com>
2025-08-07 21:40:53 +0700

    add projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,16 +10,31 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: 'Portfolio Website',
-    description: 'My personal website built with React, Tailwind, and Framer Motion.',
-    image: '/images/portfolio.png',
-    link: 'https://myportfolio.com',
-    techStack: ['React', 'TypeScript', 'Tailwind CSS', 'Framer Motion']
+    title: "Bean scene",
+    description: "Coffee shop website",
+    image: "/Bean-scene-thumbnail.png",
+    link: "https://bean-scene-9y3334yhc-franks-projects-e487ec0a.vercel.app/",
+    techStack: ["Next.js", "Shadcn", "Tailwind CSS", "Payload cms", "mongodb"],
   },
   {
-    title: 'E-commerce Store',
-    description: 'A simple store front with shopping cart functionality.',
-    image: '/images/store.png',
-    techStack: ['Next.js', 'Stripe', 'Tailwind CSS']
-  }
+    title: "TechD",
+    description:
+      "Nextjs smartPhone ecommerce website with responsive layout and clerk authentication.",
+    image: "/TechD_Thumbnail.png",
+    link: "https://techd-six.vercel.app/",
+    techStack: [
+      "Nextjs",
+      "TypeScript",
+      "Tailwind CSS",
+      "Framer Motion",
+      "Clerk",
+    ],
+  },
+  {
+    title: "Positivus",
+    description: "Digital Marketing website",
+    image: "/Positivus-hero-img.png",
+    link: "https://positivus-six-mu.vercel.app/",
+    techStack: ["Next.js", "Shadcn", "Tailwind CSS"],
+  },
 ];
